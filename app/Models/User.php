3db4def21fddb2b0cdf0e5f028214436ab0325e2
YasminFred <yasmin.frederico@estudante.ifto.edu.br>
2025-02-27 11:38:39 -0300

    outro teste de mudanca de username

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
-    //testando mudanca de username
+    //testando outra mudanca de username
     use HasFactory, Notifiable;
 
     protected $fillable = ['company_id', 'cpf', 'name', 'email', 'password', 'phone', 'is_verified', 'role'];
