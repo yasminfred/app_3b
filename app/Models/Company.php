804c031b39117a52d11bf21b44383f6c92c5442a
yasmin_frederico <ariel.barros@estudante.ifto.edu.br>
2025-02-27 11:19:15 -0300

    testando

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    // Testando mudança do usuario
+    use HasFactory;
+
+    protected $fillable = ['cnpj', 'company_name', 'address', 'phone'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
