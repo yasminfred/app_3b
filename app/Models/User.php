1ae37bbeebe972d5189ba10116989c259d5557e2
yasmin_frederico <ariel.barros@estudante.ifto.edu.br>
2025-02-27 11:32:50 -0300

    tst username

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
+    //testando mudanca de username
     use HasFactory, Notifiable;
 
     protected $fillable = ['company_id', 'cpf', 'name', 'email', 'password', 'phone', 'is_verified', 'role'];
